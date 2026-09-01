1cd7285bc51f38c66b5616f3b56977e1208d0df1
Rafael de Aquino <rafaeldeaquinoy@gmail.com>
2026-08-31 22:48:39 -0300

    fix: valida e organiza historico diario

--- src/historico.cpp
+++ src/historico.cpp
@@ -1,81 +1,74 @@
 #include <Arduino.h>
+
 #include "agua.h"
+#include "config.h"
 #include "energia.h"
+#include "historico.h"
 
-const int MAX_DIAS_MES = 31;
-int diasNoMes = 30;
+static int diaAtual = 1;
+static unsigned long momentoInicioDia = 0;
+static bool historicoAtivo = true;
 
-int diaatual = 1;
-unsigned long momentoInicioDia;
-
-float aguapdia[MAX_DIAS_MES];
-float energiapdia[MAX_DIAS_MES];
+static float aguaPorDia[HISTORY_MAX_DAYS] = {};
+static float energiaPorDia[HISTORY_MAX_DAYS] = {};
 
 void iniciarHistorico() {
-  momentoInicioDia = millis();
+    diaAtual = 1;
+    momentoInicioDia = millis();
+    historicoAtivo = true;
+
+    for (int i = 0; i < HISTORY_MAX_DAYS; i++) {
+        aguaPorDia[i] = 0.0f;
+        energiaPorDia[i] = 0.0f;
+    }
 }
 
-bool historicoAtivo = true;
-
 int verificarDia() {
-  if (!historicoAtivo) {
-    return 0;
-  }
+    if (!historicoAtivo) {
+        return 0;
+    }
 
-  unsigned long tempoAtual = millis();
-  unsigned long tempoDecorrido = tempoAtual - momentoInicioDia;
+    unsigned long tempoAtual = millis();
+    unsigned long tempoDecorrido = tempoAtual - momentoInicioDia;
 
-  if (tempoDecorrido >= 10000) {
-    int diaFinalizado = diaatual;
-    aguapdia[diaatual - 1] = obterLitros();
-    energiapdia[diaatual - 1] = obterEnergia();
+    if (tempoDecorrido < SIMULATED_DAY_DURATION_MS) {
+        return 0;
+    }
 
-    Serial.print("Dia ");
-    Serial.print(diaatual);
-    Serial.println(" finalizado!");
+    int diaFinalizado = diaAtual;
+    aguaPorDia[diaFinalizado - 1] = obterLitros();
+    energiaPorDia[diaFinalizado - 1] = obterEnergia();
 
-    if (diaatual < diasNoMes) {
-      diaatual++;
-      momentoInicioDia = tempoAtual;
-
-      resetarConsumoDiarioEnergia();
-      resetarConsumoDiarioAgua();
-
+    if (diaAtual < SIMULATED_DAYS_IN_MONTH) {
+        diaAtual++;
+        momentoInicioDia = tempoAtual;
+        resetarConsumoDiarioAgua();
+        resetarConsumoDiarioEnergia();
     } else {
-      historicoAtivo = false;
-
-      Serial.println("Limite de dias atingido. Historico completo.");
+        historicoAtivo = false;
     }
 
     return diaFinalizado;
-  }
-  return 0;
 }
 
 float obterAguaDoDia(int dia) {
-  if (dia < 1 || dia > diasNoMes) {
-    return -1.0;
-  }
-
-  return aguapdia[dia - 1];
+    if (dia < 1 || dia > obterDiasFinalizados()) {
+        return -1.0f;
+    }
+    return aguaPorDia[dia - 1];
 }
 
 float obterEnergiaDoDia(int dia) {
-  if (dia < 1 || dia > diasNoMes) {
-    return -1.0;
-  }
-
-  return energiapdia[dia - 1];
+    if (dia < 1 || dia > obterDiasFinalizados()) {
+        return -1.0f;
+    }
+    return energiaPorDia[dia - 1];
 }
 
 int obterDiasFinalizados() {
-  if (historicoAtivo) {
-    return diaatual - 1;
-  }
-
-  return diasNoMes;
+    return historicoAtivo ? diaAtual - 1 : SIMULATED_DAYS_IN_MONTH;
 }
 
 int obterDiasNoMes() {
-  return diasNoMes;
+    return SIMULATED_DAYS_IN_MONTH;
 }
