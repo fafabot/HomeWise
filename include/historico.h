c093e5eb2dce37d44d6dac32d549524d972e0ae6
Rafael de Aquino <rafaeldeaquinoy@gmail.com>
2026-08-31 22:48:34 -0300

    fix: protege interface do historico

--- include/historico.h
+++ include/historico.h
@@ -1,6 +1,12 @@
+#ifndef HISTORICO_H
+#define HISTORICO_H
+
 void iniciarHistorico();
 int verificarDia();
+
 float obterAguaDoDia(int dia);
 float obterEnergiaDoDia(int dia);
 int obterDiasFinalizados();
 int obterDiasNoMes();
+
+#endif
