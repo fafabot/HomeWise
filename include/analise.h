2d258efb2a8519da099169aac69b0dd895130e22
Rafael de Aquino <rafaeldeaquinoy@gmail.com>
2026-08-31 22:48:35 -0300

    fix: completa interface de analise

--- include/analise.h
+++ include/analise.h
@@ -1,3 +1,6 @@
+#ifndef ANALISE_H
+#define ANALISE_H
+
 float calcularMediaAgua();
 float calcularMediaEnergia();
 
@@ -5,6 +8,10 @@ float calcularPrevisaoMensalAgua();
 float calcularPrevisaoMensalEnergia();
 
 float calcularMediaAguaAntesDoDia(int dia);
+float calcularMediaEnergiaAntesDoDia(int dia);
 
+bool possuiBaseComparacao(int dia);
 bool consumoAguaAnormal(int dia);
 bool consumoEnergiaAnormal(int dia);
+
+#endif
